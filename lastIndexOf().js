29b262c7143adc95bc5e64c0f8e8ac1984ecf246
JingWen1113 <48634970+JingWen1113@users.noreply.github.com>
2020-06-03 01:23:24 -0400

    lastIndexOf()

---
+++ lastIndexOf().js
@@ -0,0 +1,15 @@
+/*Without using the native “Array.prototype.lastIndexOf” method
+ of JavaScript, compose a function titled “myUnshift” (typo) that 
+ will take in an array of elements and returns the index of the last 
+ encounter of a target element (if it is found) or -1 if that element
+  does not exist within the input array.
+ */
+
+ Array.prototype.myUnshift(callback,lastEncounterTargetIndex){
+    var i;
+    for(i = this.length-1;i=0;i-- ){
+        if(callback.call(this[i],i,this)===lastEncounterTargetIndex)    
+        return lastEncounterTargetIndex;
+    }
+    return -1;
+ };
