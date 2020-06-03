5d4e2a97c8aa4112683624f73f5551489bc11c0a
JingWen1113 <48634970+JingWen1113@users.noreply.github.com>
2020-06-03 01:15:36 -0400

    indexOf()

---
+++ indexOf().js
@@ -0,0 +1,18 @@
+/*Without using the native “Array.prototype.indexOf” method of 
+JavaScript, compose a function titled “myIndexOf” that will take in an 
+array of elements and returns the index of the first encounter of a
+ target element (if it is found) or -1 if that element does not exist 
+ within the input array.
+ */
+
+ Array.prototype.myIndexOf(callback, targetElement){
+    
+    var i;
+    for(i =0; i<this.length; i++){
+        if( callback.call(targetElement,this[i],i,this)){
+            
+            return i;
+        }   
+    }
+    return -1;         
+ };
