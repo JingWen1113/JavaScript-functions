6019793d5242652d2ab3e47f74a41e0472c6106e
JingWen1113 <48634970+JingWen1113@users.noreply.github.com>
2020-06-03 00:45:08 -0400

    Add files via upload

---
+++ every().js
@@ -0,0 +1,13 @@
+/**Without using the native “Array.prototype.every” method of JavaScript,
+ *  compose a function titled “myEvery” that will take in an array of elements 
+ * and executes a callback function on each of those elements.
+ */
+
+
+Array.prototype.myEvery(){
+    var i;
+    for(i = 0; i<this.length; i++){
+        if(!callback.call(condition,this[i],i,this))    return false;
+    }
+    return true;
+};
