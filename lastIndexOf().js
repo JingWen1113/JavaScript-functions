19cf619dbbe1623a9c4b95f674830aebbe463b1e
JingWen1113 <48634970+JingWen1113@users.noreply.github.com>
2020-06-04 00:29:55 -0400

    add another possible implementation

--- lastIndexOf().js
+++ lastIndexOf().js
@@ -5,7 +5,7 @@
   does not exist within the input array.
  */
 
- Array.prototype.myUnshift = function(callback,lastEncounterTargetIndex){
+ /*Array.prototype.myUnshift = function(callback,lastEncounterTargetIndex){
     var i;
     for(i = this.length-1;i=0;i-- ){
         if(callback.call(this[i],i,this)===lastEncounterTargetIndex)    
@@ -13,3 +13,20 @@
     }
     return -1;
  };
+ */
+
+
+function myLastIndexOf(anArray, targetElement, currentPosition=0){
+  var {length} = array;
+  var index = -1;
+ 
+ if(currentPosition <0 || currentPosition >= length){
+  currentPosition = 0;
+ }
+ for( var i =currentPosition; i<this.length;i++){
+   index = i;
+ }
+ return index;
+};
+
+console.log(myLastIndexOf(a,8));//call the function and print out
